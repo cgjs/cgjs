31dbb4d84025785fe92b226b885f3500fdeffda7
Sonny Piers <sonny@fastmail.net>
2018-04-16 16:16:34 +0200

    import @cgjs/import

---
+++ packages/assert/test/index.js
@@ -0,0 +1,19 @@
+const assert = require('../index');
+
+const obj1 = {
+  a: {
+    b: 1
+  }
+};
+const obj2 = {
+  a: {
+    b: 2
+  }
+};
+const obj3 = {
+  a: {
+    b: 1
+  }
+};
+
+assert.deepEqual(obj1, obj3);
